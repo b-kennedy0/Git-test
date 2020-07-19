99d2bf1c9484515946f29ded4d43cad7232a072c
b-kennedy0 <bradley.james@me.com>
2020-07-19 21:48:02 +0000

    Updated plot and avoided error by column not existing before the for loop

--- Log-analysis.R
+++ Log-analysis.R
@@ -6,11 +6,12 @@ colnames(data) <- c("date")
 data$date <- dmy_hm(data$date)
 p <- qplot(data$date, bins=200, binwidth=2500) + xlab("Date")
 p
+data$time_since <- NA
 for (row in 2:nrow(data)){
   data$time_since[row] <- as.numeric(difftime(strptime(data$date[row],"%Y-%m-%d %H:%M:%S"),
                                             strptime(data$date[row-1], "%Y-%m-%d %H:%M:%S"), units = "hours"))
 }
 mean_time_since <- mean(data$time_since, na.rm = TRUE)
-q <- ggplot(data, aes(date,time_since)) + geom_point() + 
+q <- ggplot(data, aes(date,time_since)) + geom_point() + geom_line() +
   geom_hline(yintercept=mean_time_since, linetype="dashed", color = "red")
 q
